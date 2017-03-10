28fc55f21460d8a300537424b7d039887bab724d
Chris Jones <cj@hanscj.com>
2017-03-10 12:45:50 -0500

    Updated docs and point release

--- src/lib.rs
+++ src/lib.rs
@@ -12,6 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// NOTE: This attribute only needs to be set once.
+#![doc(html_logo_url = "https://lambdastackio.github.io/static/images/lambdastack-200x200.png",
+       html_favicon_url = "https://lambdastackio.github.io/static/images/favicon.ico",
+       html_root_url = "https://lambdastackio.github.io/azure-sdk-rust/azure_sdk_rust/azure/index.html")]
+
 // #![feature(plugin)]
 // #![plugin(clippy)]
 
